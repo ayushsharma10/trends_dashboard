24a2a27931ab64df29f79a3c5cfaca563513c39b
Ayush Sharma <sharma.ayush1002@gmail.com>
2019-06-26 03:12:44 +0530

    UI Modifications

--- UI.R
+++ UI.R
@@ -6,31 +6,76 @@ shinyUI(
   dashboardPage(
     dashboardHeader(title = "Team David",
                     dropdownMenu(type = "message",
-                                 messageItem(from = "Hello User", message = "Welcome to our dynamic dashboard"))
-                    ),
+                                 messageItem(from = "Hello User", message = "Welcome to our dynamic dashboard"),
+                                 messageItem(from = "Alert", message = "7+ Work Ex users have least applies", icon = icon("exclamation-circle"), time = "14:30"),
+                                 messageItem(from = "Review Meeting", message = "Meeting at 2 PM on Friday ", icon = icon("handshake-o"), time = "15:00")),
+                    
+                    dropdownMenu(type = "notification",
+                                 notificationItem(
+                                   text = "New Tabs added to Dashboard",
+                                   icon = icon("dashboard"),
+                                   status = "success"),
+                                 
+                                 notificationItem(
+                                   text = "More jobs accquisitions required",
+                                   icon = icon("warning"),
+                                   status = "warning"
+                                 )
+                                 ),
+                    dropdownMenu( type = "tasks",
+                                  taskItem(
+                                    value = 80,
+                                    color = "aqua",
+                                    "Current CV accquisitions", #Monthly target completion
+                                  ),
+                                  
+                                  taskItem(
+                                    value = 55,
+                                    color = "red",
+                                    "Current Job accquisitions", ##Monthly target completion 
+                                  ),
+                                  
+                                  taskItem(
+                                    value = 30,
+                                    color = "green",
+                                    "Engagement Activities"
+                                    
+                                  )
+                                  
+                      
+                    )  
+                    
+                  ),
     
     
-    dashboardSidebar(sliderInput("bins", "Years of experience", 1,50,10),br(),
-                     
+    dashboardSidebar(
                      sidebarMenu(
+                       
+                       sidebarSearchForm(" searchText", "buttonSearch", "Search"), br(),
                      menuItem("Jobseeker Info", tabName = "JSinfo", icon = icon("user-circle")), 
                      
                      menuItem("Job Info", tabName = "jobs", icon = icon("briefcase")),
                      
                      menuItem("Domain", tabName = "domain", icon= icon("search")), 
-                     menuSubItem("Trends", icon = icon("chart-line"))
-                     )),
+                     menuItem("Trends",tabName = "trend", icon = icon("chart-line"), badgeLabel = "beta", badgeColor = "yellow")
+                     ),br(),
+                     
+               
+                           sliderInput("bins", "Years of experience", 1,50,10),
+                           textInput("text_input","Search User", value = "")),
     dashboardBody(
       
       tabItems(
         tabItem(tabName = "JSinfo", 
-                fluidRow(
-                  box(plotOutput("histogram"))
-                )),
+                h1("Job Seeker Related Information Here")),
         tabItem(tabName = "jobs", 
                 h1("Job Related Information Here")),
         tabItem(tabName = "domain",
-                h1("Domain Specific Information Here"))
+                h1("Domain Specific Information Here")),
+        tabItem(tabName = "trend", 
+                fluidRow(
+                  box(plotOutput("histogram"))
+                ))
            
         )
       )
--- server.R
+++ server.R
@@ -5,7 +5,9 @@ shinyServer(
   function (input,output){
     
     output$histogram <- renderPlot(
-      hist(iris$Sepal.Length, breaks = input$bins)
+      hist(iris$Sepal.Length, breaks = input$bins, main = "Job Applies W.R.T. Years of Experience",
+           xlab = "Years of Experience",
+           ylab = "Total Applies")
     )
   }
 )

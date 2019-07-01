781c37a17155bc8cbdeec588d25128dcd9df9062
Ayush Sharma <sharma.ayush1002@gmail.com>
2019-07-01 21:05:37 +0530

    Jobs Page added

--- UI.R
+++ UI.R
@@ -1,16 +1,22 @@
 #install.packages("shinydashboard")
 library(shiny)
 library(shinydashboard)
-iris1 <- data(iris)
+
 shinyUI(
   dashboardPage(
     dashboardHeader(title = "Team David",
-                    dropdownMenu(type = "message",
+                    dropdownMenu(type = "message", 
+                                 
+                                 
+                                 #Messages
                                  messageItem(from = "Hello User", message = "Welcome to our dynamic dashboard"),
                                  messageItem(from = "Alert", message = "7+ Work Ex users have least applies", icon = icon("exclamation-circle"), time = "14:30"),
                                  messageItem(from = "Review Meeting", message = "Meeting at 2 PM on Friday ", icon = icon("handshake-o"), time = "15:00")),
                     
                     dropdownMenu(type = "notification",
+                                 
+                                 
+                                 #Notifications
                                  notificationItem(
                                    text = "New Tabs added to Dashboard",
                                    icon = icon("dashboard"),
@@ -22,7 +28,11 @@ shinyUI(
                                    status = "warning"
                                  )
                                  ),
+                    
                     dropdownMenu( type = "tasks",
+
+                                  
+                                  #Targets
                                   taskItem(
                                     value = 80,
                                     color = "aqua",
@@ -32,7 +42,7 @@ shinyUI(
                                   taskItem(
                                     value = 55,
                                     color = "red",
-                                    "Current Job accquisitions", ##Monthly target completion 
+                                    "Current Job accquisitions", #Monthly target completion 
                                   ),
                                   
                                   taskItem(
@@ -52,13 +62,13 @@ shinyUI(
                      
                      #SideBar Menu begins
                      sidebarMenu(
-                       
-                       sidebarSearchForm(" searchText", "buttonSearch", "Search"), br(),
+                       br(),
+                      # sidebarSearchForm(" searchText", "buttonSearch", "Search"), br(),
                      menuItem("Jobseeker Info", tabName = "JSinfo", icon = icon("user-circle")), 
                      
                      menuItem("Job Info", tabName = "jobs", icon = icon("briefcase")),
                      
-                     menuItem("Domain", tabName = "domain", icon= icon("search")), 
+                     #menuItem("Domain", tabName = "domain", icon= icon("search")), 
                      menuItem("Trends",tabName = "trend", icon = icon("chart-line"), badgeLabel = "beta", badgeColor = "yellow")
                      
                      #Sidebar Menu Ends
@@ -77,22 +87,57 @@ shinyUI(
                           
                      ),
     dashboardBody(
-      
+      tags$style(".small-box.bg-yellow { background-color: #FFFF00 !important; color: #000000 !important; }"),
       tabItems(
+        
+        
+        #Jobseeker Tab
         tabItem(tabName = "JSinfo", 
                 h1("Job Seeker Related Information"), br(),
                 fluidRow(
                   tabBox(width = 400,
-                  tabPanel(title= 'Jobseekers Information', icon = icon("user")),
-                  tabPanel(title= "Views and Applies",icon = icon("check-circle"),box(title = "Status of Viewed and Applied Jobs by Jobseeker",width = 400, status = "primary", solidHeader = T, DT::dataTableOutput('items_dt')))
+                  tabPanel(title= 'Jobseekers Information', icon = icon("user"),box(title = "Jobseeker Mandate",width = 400, status = "primary", solidHeader = T, DT::dataTableOutput('js_info'))),
+                  tabPanel(title= "Views and Applies",icon = icon("check-circle"),box(title = "Status of Viewed and Applied Jobs by Jobseeker",width = 400, status = "primary", solidHeader = T, DT::dataTableOutput('views_applies')))
                   
                   ))
         
         ),
-                tabItem(tabName = "jobs", 
-                h1("Job Related Information"), h3("Coming soon...")),
-        tabItem(tabName = "domain",
-                h1("Domain Specific Information"), h3("Coming soon...")),
+                
+        
+        #Jobs Tab
+        tabItem(tabName = "jobs", 
+                
+                fluidRow(
+                  valueBox(paste0(BE, ' Active Jobs'), "Backend", color = "blue", icon = icon('python'), width = 3 ),
+                  valueBox(paste0(FE, ' Active Job') , "FrontEnd", color = "orange", icon = icon('js'), width = 3 ),
+                  valueBox(paste0(FS, ' Active Jobs') , "Full Stack", color = "green", icon = icon('file-code'), width = 3 ),
+                  valueBox(paste0(Mob, ' Active Jobs') , "Mobile", color = "teal", icon = icon('android'), width = 3 )
+                  #,
+                  #valueBox( '0 Active Jobs' , "DevOps", color = "red", icon = icon('aws'), width = 3 ),
+                  #valueBox( '0 Active Jobs' , "Datawarehousing", color = "maroon", icon = icon('database'), width = 3 )
+                ),
+                fluidRow(
+                  tabBox(width = 400,
+                         tabPanel(title= 'Jobs Information', icon = icon("building"),
+                                  box(title = "Job Mandate",width = 700, status = "primary", solidHeader = T, DT::dataTableOutput('jobsinfo')))
+                
+                
+              ))),
+        
+        
+        
+        #Domain Tab
+        # tabItem(tabName = "JS Info",
+        #         h1("Domain Summary"),
+        #         fluidRow(
+        #           valueBox(paste0(BE, ' Active Jobs') , "Backend", color = "blue", icon = icon('python'), width = 3 ),
+        #           valueBox(paste0(FE, ' Active Job') , "FrontEnd", color = "yellow", icon = icon('js'), width = 3 ),
+        #           valueBox(paste0(FS, ' Active Jobs') , "Full Stack", color = "green", icon = icon('file-code'), width = 3 ),
+        #           valueBox(paste0(Mob, ' Active Jobs') , "Mobile", color = "teal", icon = icon('android'), width = 3 )
+        #         )),
+        
+        
+        #Trends Tab
         tabItem(tabName = "trend", 
                 fluidRow(
                   box(plotOutput("histogram"), title = 'Applies to Work Experience', status = 'primary', background = "aqua"  ,solidHeader = T),
--- server.R
+++ server.R
@@ -1,5 +1,14 @@
 library(shiny)
 library(shinydashboard)
+library(DT)
+
+
+FE <- as.numeric(length(which(jobMandate$Role == "Frontend")))
+BE <- as.numeric(length(which(jobMandate$Role == "Backend")))
+FS <- as.numeric(length(which(jobMandate$Role == "Full Stack")))
+Mob <- as.numeric(length(which(jobMandate$Role == "Mobile")))
+
+
 
 shinyServer(
   function (input,output){
@@ -9,12 +18,36 @@ shinyServer(
            xlab = "Years of Experience",
            ylab = "Total Applies")
     )
-    
-    output$items_dt = DT::renderDataTable(
-      JS360view,
+
+    output$views_applies = DT::renderDataTable(
+      views_applies, extensions = 'Buttons',
       filter = 'top',
-      options = list(scrollX = TRUE), editable = T , colnames = c('Email Id','Job ID','Company Name','Job Title','View Date','Apply Date', 'Application Status')
+      options = list(scrollX = TRUE, processing = F,
+                     dom = 'Bfrtip', buttons = I('colvis')
+                     ),
+      editable = T ,rownames = F , colnames = c('Email Id','Job ID','Company Name','Job Title','View Date','Apply Date', 'Application Status')
     )
+
+    output$js_info = DT::renderDataTable(
+      jsprofile,
+      filter = 'top', rownames = F , extensions = 'Buttons',
+      options = list(scrollX = TRUE
+                     , dom = 'Bfrtip',process = F , buttons = I('colvis')),
+      editable = T)
+    
+    
+    
+    output$jobsinfo = DT::renderDataTable(
+      jobMandate,
+      filter = 'top',extensions = 'Buttons',
+      options = list( columnDefs = list(list(className = 'dt-center', targets = c(3:6, 13,14))),
+                      dom = 'Bfrtip', buttons = I('colvis'),scrollX = TRUE
+                      ),
+      
+        
+      editable = T, rownames = F ,
+      colnames = c('Company','Job Title','Domain','Min CTC','Max CTC','Min Work Ex', 'Max Work Ex',
+                                                                  'Location', 'Category', 'Status','Date', 'Spoc', 'Mandatory Skills', 'Veiws','Applies'))
     
     }
 )
